dced9e7de1c90456618de171aaa940c5f0625316
Witold Mescheryakov <witcold@users.noreply.github.com>
2013-12-18 14:58:24 -0800

    Update UProcess.pas
    Рефакторинг

--- UProcess.pas
+++ UProcess.pas
@@ -1,26 +1,28 @@
 unit UProcess;
 
 interface
+
 type
-  TArr = array [1..400] of real;
-  procedure Smooth(nv : integer; y : TArr; var z  : TArr; count : integer);
+  TType = Real;
+  TVector = array [1..100] of TType;
+  procedure Smooth(NV: Integer; var Y, Z: TVector; Count: Integer);
 
 implementation
 
-// выполнение сглаживание функции, заданной точками y. результат помещается в z;
-// count содержит количество повторений сглаживаний
-procedure Smooth(nv : integer; y : TArr; var z  : TArr; count : integer);
-var i, r : integer;
+// Cглаживание функции, заданной точками из Y, результат помещается в Z;
+// count содержит количество сглаживаний
+procedure Smooth(NV: Integer; var Y, Z: TVector; Count: Integer);
+var
+  i, k : integer;
 begin
-  z[1]:=y[1];
-  z[nv]:=y[nv];
-  for r:= 1 to count do begin
-    z[2]:=   (2*y[1] + 27*y[2] + 12*y[3] - 8*y[4] + 2*y[5])/35;
-    z[nv-1]:=(2*y[nv-4] - 8*y[nv-3] + 12*y[nv-2] + 27*y[nv-1] + 2*y[nv])/35;
-    for i:= 3 to nv-2 do
-      z[i]:= ((-3)*y[i-2] + 12*y[i-1] + 17*y[i] + 12*y[i+1] - 3*y[i+2])/35;
+  Z[1] := Y[1];
+  Z[NV] := Y[NV];
+  for k := 1 to Count do begin
+    Z[2] := (2*Y[1] + 27*Y[2] + 12*Y[3] - 8*Y[4] + 2*Y[5])/35;
+    Z[NV-1] := (2*Y[NV-4] - 8*Y[NV-3] + 12*Y[NV-2] + 27*Y[NV-1] + 2*Y[NV])/35;
+    for i := 3 to NV-2 do
+      Z[i] := (-3*Y[i-2] + 12*Y[i-1] + 17*Y[i] + 12*Y[i+1] - 3*Y[i+2])/35;
   end;    
 end;
 
 end.
- 
